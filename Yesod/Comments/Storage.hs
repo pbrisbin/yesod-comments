ae9c627564dfe9073ef0c70c4d7e351b1cd51842
patrick brisbin <pbrisbin@gmail.com>
2011-01-15 14:38:57 -0500

    remove useless fileDB

--- Yesod/Comments/Storage.hs
+++ Yesod/Comments/Storage.hs
@@ -12,12 +12,11 @@
 -- Stability   :  unstable
 -- Portability :  unportable
 --
--- Some pre-built backend definitions for use with 'Comment.runCommentsForm'
+-- Some pre-built backend definitions for use with runCommentsForm.
 --
 -------------------------------------------------------------------------------
 module Yesod.Comments.Storage
     ( testDB
-    , fileDB
     , persistentDB
     , migrateComments
     ) where
@@ -25,33 +24,17 @@ module Yesod.Comments.Storage
 import Yesod
 import Yesod.Comments.Core
 
-import Control.Monad    (when)
-import Data.List.Split  (wordsBy)
 import Data.Time.Clock  (UTCTime)
-import Data.Time.Format (parseTime, formatTime)
-import Data.Maybe       (mapMaybe, maybeToList)
 import System.IO        (hPutStrLn, stderr)
-import System.Locale    (defaultTimeLocale)
 
 import Database.Persist.TH         (share2)
 import Database.Persist.GenericSql (mkMigrate)
 
-import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.Char8 as L
-
--- | strict reads are required since reading/writing occur so close to
---   each other and ghc leaves the handles open
-import qualified System.IO.Strict
 
 -- | some utilities
 htmlToString :: Html -> String
 htmlToString = L.unpack . renderHtml
-
-formatTime' :: UTCTime -> String
-formatTime' = formatTime defaultTimeLocale "%s"
-
-parseTime' :: String -> Maybe UTCTime
-parseTime' = parseTime defaultTimeLocale "%s"
 
 -- | For use during testing, always loads no comments and prints the
 --   comment to stderr as the /store/ action
@@ -61,58 +44,6 @@ testDB = CommentStorage
     , loadComments  = \_   -> return []
     , deleteComment = \_ _ -> return ()
     }
-
--- | Convert a 'Comment' into a pipe-delimited string for storage as a
---   single line in a file
-toFileString :: Comment -> String
-toFileString comment = concat
-    [ threadId comment,                          "|"
-    , show $ commentId comment,                  "|"
-    , formatTime' $ timeStamp comment,           "|"
-    , ipAddress comment,                         "|"
-    , userName comment,                          "|"
-    , fixPipe . htmlToString $ content comment, "\n"
-    ]
-    where
-        -- since we use | as a delimiter we need to htmlize it before
-        -- storing the comment
-        fixPipe []         = []
-        fixPipe ('|':rest) = "&#124;" ++ fixPipe rest
-        fixPipe (x:rest)   = x : fixPipe rest
-
--- | Maybe read the same delimited string into a 'Comment'
-fromFileString :: String -> String -> Maybe Comment
-fromFileString thread str =
-    case wordsBy (=='|') str of
-        -- monadic maybe shenanigans
-        [t, c, ts, ip, u, h] -> do
-            when (t /= thread) (fail [])
-            parseTime' ts >>= \utc ->
-                return $ Comment
-                    { threadId  = t
-                    , commentId = read c :: Int
-                    , timeStamp = utc
-                    , ipAddress = ip
-                    , userName  = u
-                    , content   = preEscapedString h
-                    }
-        _ -> Nothing
-
--- | A simple flat file storage method; this is dead-simple to setup but
---   probably shouldn't be used since it's unreliable and likely to be
---   slow
-fileDB :: (Yesod m) => FilePath -> CommentStorage s m
-fileDB f = CommentStorage
-    { storeComment = liftIO . appendFile f . toFileString
-
-    , loadComments = \id -> do
-        contents <- liftIO $ System.IO.Strict.readFile f
-        return $ mapMaybe (fromFileString id) (lines contents)
-
-        -- todo: how to do this without rewriting the whole file?
-    , deleteComment = \tid cid -> undefined
-    }
-    where
 
 -- | Create the required types and migration function for use in a
 --   general yesod app
