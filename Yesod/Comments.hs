2fc58c8d2785d09d55f3a682c1ae4f8cf074a302
patrick brisbin <pbrisbin@gmail.com>
2011-04-11 11:15:13 -0400

    refactor duplicate code between auth and non auth comments

--- Yesod/Comments.hs
+++ Yesod/Comments.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE FlexibleContexts #-}
 -------------------------------------------------------------------------------
 -- |
 -- Module      :  Yesod.Comments
@@ -12,217 +13,135 @@
 -- in the early stages of development. Beware bugs, patches welcome.
 --
 -------------------------------------------------------------------------------
-module Yesod.Comments 
-    ( addComments
-    , addCommentsAuth
-    , module Yesod.Comments.Core
-    ) where
+module Yesod.Comments where
+--    ( addComments
+--    , addCommentsAuth
+--    , module Yesod.Comments.Core
+--    ) where
 
 import Yesod
 import Yesod.Comments.Core
 import Yesod.Comments.Filters (applyFilters)
 import Yesod.Helpers.Auth
 
-import Data.Time
-import System.Locale
-
-import Data.Char (isSpace)
 import Text.Blaze (toHtml)
 
--- | Add comments that anyone can enter anonymously
+-- | Comments that anyone can enter anonymously
 addComments :: YesodComments m 
             => ThreadId -- ^ the thread you're adding comments to
             -> GWidget s m ()
 addComments tid = do
-    comments <- lift $ loadComments tid
-    cid      <- lift $ getNextCommentId comments
-    
-    -- run the form
-    ((res, form), enctype) <- lift $ runFormMonadPost (commentForm Nothing)
-    case res of
-        FormMissing    -> return ()
-        FormFailure _  -> return ()
-        FormSuccess cf -> lift $ do
-            comment <- commentFromForm tid cid cf
-            matches <- applyFilters commentFilters comment
-            if matches
-                then setMessage $ toHtml "comment dropped. matched filters."
-                else do
-                    storeComment comment
-                    setMessage $ toHtml "comment added."
-            redirectCurrentRoute
+    comments               <- lift $ loadComments tid
+    cid                    <- lift $ getNextCommentId comments
+    ((res, form), enctype) <- lift $ runFormMonadPost commentForm
 
-    -- make the input box a bit bigger
-    addCassius [cassius|
-        .yesod_comment_input th
-            text-align: left
-            vertical-align: top
-        .yesod_comment_input textarea
-            height: 10ex
-            width: 50ex
-        |]
-        
-    -- show the input form
+    handleForm res tid cid
+    addStyling
     [hamlet|
         <div .yesod_comments>
             <h4>Add a comment:
             <div .yesod_comment_input>
-                <form enctype="#{enctype}" method="post">
-                    ^{form}
-                <p .helptext>
-                    Comments are parsed as pandoc-style markdown
+                <form enctype="#{enctype}" method="post">^{form}
+                <p .helptext>Comments are parsed as pandoc-style markdown
 
-            <h4>Showing #{toHtml $ helper $ length comments}:
-            $forall comment <- comments
-                <div .yesod_comment>
-                    ^{showComment comment}
-        |]
+            ^{displayComments comments}
+    |]
 
--- | Add comments that require site-wide authentication
-addCommentsAuth :: (YesodComments m, YesodAuth m)
+-- | Comments that require authentication
+addCommentsAuth :: (YesodAuth m, YesodComments m)
                 => ThreadId -- ^ the thread you're adding comments to
                 -> GWidget s m ()
 addCommentsAuth tid = do
-    (isAuthenticated, username) <- lift $ do
+    (isAuthenticated, uid, username) <- lift $ do
         muid <- maybeAuthId
         case muid of
-            Nothing  -> return (False, "")
+            Nothing  -> return (False, "", "")
             Just uid -> do
+                uids  <- fmap (flip showAuthId uid) getYesod
                 uname <- displayUser uid
-                return (True, uname)
+                return (True, uids, uname)
 
-    comments <- lift $ loadComments tid
-    cid      <- lift $ getNextCommentId comments
-    
-    -- run the form
-    ((res, form), enctype) <- lift $ runFormMonadPost (commentForm $ Just username)
-    case res of
-        FormMissing    -> return ()
-        FormFailure _  -> return ()
-        FormSuccess cf -> lift $ do
-            comment <- commentFromForm tid cid cf
-            matches <- applyFilters commentFilters comment
-            if matches
-                then setMessage $ toHtml "comment dropped. matched filters."
-                else do
-                    storeComment comment
-                    setMessage $ toHtml "comment added."
-            redirectCurrentRoute
+    comments               <- lift $ loadComments tid
+    cid                    <- lift $ getNextCommentId comments
+    ((res, form), enctype) <- lift $ runFormMonadPost $ commentFormAuth uid username
 
-    -- make the input box a bit bigger
-    addCassius [cassius|
-        .yesod_comment_input th
-            text-align: left
-            vertical-align: top
-        .yesod_comment_input textarea
-            height: 10ex
-            width: 50ex
-        |]
-        
-    -- show the input form
+    handleForm res tid cid
+    addStyling
     [hamlet|
         <div .yesod_comments>
             $if isAuthenticated
                 <h4>Add a comment:
                 <div .yesod_comment_input>
-                    <form enctype="#{enctype}" method="post">
-                        ^{form}
-                    <p .helptext>
-                        Comments are parsed as pandoc-style markdown
+                    <form enctype="#{enctype}" method="post">^{form}
+                    <p .helptext>Comments are parsed as pandoc-style markdown
             $else
-                <h4>Please 
-                    ^{loginLink}
-                    \ to post a comment.
+                <h4>Please ^{login} to post a comment.
 
-            $if not $ null comments
-                <h4>Showing #{toHtml $ helper $ length comments}:
-                $forall comment <- comments
-                    <div .yesod_comment>
-                        ^{showComment comment}
-        |]
+            ^{displayComments comments}
+    |]
 
-loginLink :: Yesod m => GWidget s m ()
-loginLink = do
+
+-- | Add styling common to the auth and non-auth forms
+addStyling :: Yesod m => GWidget s m ()
+addStyling = addCassius [cassius|
+    .yesod_comment_input th
+        text-align: left
+        vertical-align: top
+    .yesod_comment_input textarea
+        height: 10ex
+        width: 50ex
+    |]
+
+-- | Handle the posted form and actually insert the comment
+handleForm :: YesodComments m 
+           => FormResult CommentForm 
+           -> ThreadId 
+           -> CommentId 
+           -> GWidget s m ()
+handleForm res tid cid = case res of
+    FormMissing    -> return ()
+    FormFailure _  -> return ()
+    FormSuccess cf -> lift $ do
+        comment <- commentFromForm tid cid cf
+        matches <- applyFilters commentFilters comment
+        if matches
+            then setMessage $ toHtml "comment dropped. matched filters."
+            else do
+                storeComment comment
+                setMessage $ toHtml "comment added."
+        redirectCurrentRoute
+
+    where
+        -- | Redirect back to the current route after a POST request
+        redirectCurrentRoute :: Yesod m => GHandler s m ()
+        redirectCurrentRoute = do
+            tm <- getRouteToMaster
+            mr <- getCurrentRoute
+            case mr of
+                Just r  -> redirect RedirectTemporary $ tm r
+                Nothing -> notFound
+
+displayComments :: Yesod m => [Comment] -> GWidget s m ()
+displayComments comments = [hamlet|
+        $if not $ null comments
+
+            <h4>Showing #{toHtml $ helper $ length comments}:
+
+            $forall comment <- comments
+                <div .yesod_comment>^{showComment comment}
+    |]
+
+    where
+        helper :: Int -> String
+        helper 0 = "no comments"
+        helper 1 = "1 comment"
+        helper n = show n ++ " comments"
+
+-- | Show the authroute as a link if set up
+login :: Yesod m => GWidget s m ()
+login = do
     lift $ setUltDest' -- so we come back here after login
     mroute <- lift $ fmap authRoute getYesod
     case mroute of
         Just r  -> [hamlet|<a href="@{r}">log in|]
-        Nothing -> [hamlet|log in|] -- this shouldn't happen
-
-helper :: Int -> String
-helper 0 = "no comments"
-helper 1 = "1 comment"
-helper n = show n ++ " comments"
-
--- | Redirect back to the current route after a POST request
-redirectCurrentRoute :: Yesod m => GHandler s m ()
-redirectCurrentRoute = do
-    tm <- getRouteToMaster
-    mr <- getCurrentRoute
-    case mr of
-        Just r  -> redirect RedirectTemporary $ tm r
-        Nothing -> notFound
-    
--- | Show a single comment, provides numbered anchors
-showComment :: Yesod m => Comment -> GWidget s m ()
-showComment comment =  do
-    commentContent   <- lift . markdownToHtml $ content comment
-    commentTimestamp <- return . flip humanReadableTimeDiff (timeStamp comment) =<< liftIO getCurrentTime
-    let anchor = "#comment_" ++ show (commentId comment)
-    addHamlet [hamlet|
-        <p>
-            <a href="#{anchor}" id="#{anchor}">#{commentTimestamp}
-            , #{userName comment} wrote:
-
-        <blockquote>
-            #{commentContent}
-        |]
-
--- <https://github.com/snoyberg/haskellers/blob/master/Haskellers.hs>
--- <https://github.com/snoyberg/haskellers/blob/master/LICENSE>
-humanReadableTimeDiff :: UTCTime     -- ^ current time
-                      -> UTCTime     -- ^ old time
-                      -> String
-humanReadableTimeDiff curTime oldTime =
-    helper diff
-  where
-    diff    = diffUTCTime curTime oldTime
-
-    minutes :: NominalDiffTime -> Double
-    minutes n = realToFrac $ n / 60
-
-    hours :: NominalDiffTime -> Double
-    hours   n = minutes n / 60
-
-    days :: NominalDiffTime -> Double
-    days    n = hours n / 24
-
-    weeks :: NominalDiffTime -> Double
-    weeks   n = days n / 7
-
-    years :: NominalDiffTime -> Double
-    years   n = days n / 365
-
-    i2s :: RealFrac a => a -> String
-    i2s n = show m where m = truncate n :: Int
-
-    old = utcToLocalTime utc oldTime
-
-    trim = f . f where f = reverse . dropWhile isSpace
-
-    dow           = trim $! formatTime defaultTimeLocale "%l:%M %p on %A" old
-    thisYear      = trim $! formatTime defaultTimeLocale "%b %e" old
-    previousYears = trim $! formatTime defaultTimeLocale "%b %e, %Y" old
-
-    helper  d | d < 1          = "just now"
-              | d < 60         = i2s d ++ " seconds ago"
-              | minutes d < 2  = "one minute ago"
-              | minutes d < 60 =  i2s (minutes d) ++ " minutes ago"
-              | hours d < 2    = "one hour ago"
-              | hours d < 24   = "about " ++ i2s (hours d) ++ " hours ago"
-              | days d < 5     = "at " ++ dow
-              | days d < 10    = i2s (days d)  ++ " days ago"
-              | weeks d < 2    = i2s (weeks d) ++ " week ago"
-              | weeks d < 5    = i2s (weeks d)  ++ " weeks ago"
-              | years d < 1    = "on " ++ thisYear
-              | otherwise      = "on " ++ previousYears
+        Nothing -> [hamlet|log in|]
--- Yesod/Comments/Core.hs
+++ Yesod/Comments/Core.hs
@@ -18,6 +18,10 @@ import Yesod.Markdown -- my fork, <https://github.com/pbrisbin/yesod-markdown> r
 import Yesod.Form.Core
 import Yesod.Helpers.Auth
 
+import Data.Time
+import System.Locale
+
+import Data.Char           (isSpace)
 import Control.Applicative ((<$>), (<*>))
 import Data.Time.Clock     (UTCTime, getCurrentTime)
 import Network.Wai         (remoteHost)
@@ -68,38 +72,6 @@ markdownToHtml = (writePandoc yesodDefaultWriterOptions <$>)
                . localLinks 
                . parseMarkdown yesodDefaultParserState
 
--- | The comment form itself
-commentForm :: Maybe String -> GFormMonad s m (FormResult CommentForm, GWidget s m ())
-commentForm muname = do
-    (user   , fiUser   ) <- stringField   "name:"    muname
-    (comment, fiComment) <- markdownField "comment:" Nothing
-    return (CommentForm <$> user <*> comment, [hamlet|
-        <table>
-            ^{fieldRow fiUser}
-            ^{fieldRow fiComment}
-            <tr>
-                <td>&nbsp;
-                <td colspan="2">
-                    <input type="submit" value="Add comment">
-        |])
-    where
-        fieldRow fi = [hamlet|
-            <tr .#{toHtml (clazz fi)}>
-                <th>
-                    <label for="#{fiIdent fi}">#{fiLabel fi}
-                    <div .tooltip>#{fiTooltip fi}
-                <td>
-                    ^{fiInput fi}
-                <td>
-                    $maybe error <- fiErrors fi
-                        #{error}
-                    $nothing
-                        &nbsp;
-            |]
-
-
-        clazz fi = if fiRequired fi then "required" else "optional" :: String
-
 -- | Cleanse form input and create a 'Comment' to be stored
 commentFromForm :: ThreadId -> CommentId -> CommentForm -> GHandler s m Comment
 commentFromForm tid cid cf = do
@@ -113,3 +85,137 @@ commentFromForm tid cid cf = do
         , userName  = formUser cf
         , content   = formComment cf
         }
+
+-- | The comment form itself
+commentForm :: GFormMonad s m (FormResult CommentForm, GWidget s m ())
+commentForm = do
+    (user   , fiUser   ) <- stringField   "name:"    Nothing
+    (comment, fiComment) <- markdownField "comment:" Nothing
+    return (CommentForm <$> user <*> comment, [hamlet|
+        <table>
+            ^{fieldRow fiUser}
+            ^{fieldRow fiComment}
+            <tr>
+                <td>&nbsp;
+                <td colspan="2">
+                    <input type="submit" value="Add comment">
+        |])
+
+-- | The comment form if using authentication (uid is hidden and display
+--   name is shown 
+commentFormAuth :: String -> String -> GFormMonad s m (FormResult CommentForm, GWidget s m ())
+commentFormAuth uid username = do
+    (user   , fiUser   ) <- hiddenField   "name:"    (Just $ uid)
+    (comment, fiComment) <- markdownField "comment:" Nothing
+    return (CommentForm <$> user <*> comment, [hamlet|
+        <table>
+            ^{fieldRow fiUser}
+
+            <tr>
+                <th>name:
+                <td colspan="2">#{username}
+
+            ^{fieldRow fiComment}
+            <tr>
+                <td>&nbsp;
+                <td colspan="2">
+                    <input type="submit" value="Add comment">
+        |])
+
+fieldRow :: FieldInfo s m -> GWidget s m ()
+fieldRow fi = [hamlet|
+    <tr .#{toHtml (clazz fi)}>
+        <th>
+            <label for="#{fiIdent fi}">#{fiLabel fi}
+            <div .tooltip>#{fiTooltip fi}
+        <td>
+            ^{fiInput fi}
+        <td>
+            $maybe error <- fiErrors fi
+                #{error}
+            $nothing
+                &nbsp;
+    |]
+
+
+clazz :: FieldInfo s m -> String
+clazz fi = if fiRequired fi then "required" else "optional"
+
+-- | Show a single comment, provides numbered anchors
+showComment :: Yesod m => Comment -> GWidget s m ()
+showComment comment =  do
+    commentContent   <- lift . markdownToHtml $ content comment
+    commentTimestamp <- return . flip humanReadableTimeDiff (timeStamp comment) =<< liftIO getCurrentTime
+    let anchor = "#comment_" ++ show (commentId comment)
+    addHamlet [hamlet|
+        <p>
+            <a href="#{anchor}" id="#{anchor}">#{commentTimestamp}
+            , #{userName comment} wrote:
+
+        <blockquote>
+            #{commentContent}
+        |]
+
+-- | Show a single comment, provides numbered anchors
+showCommentAuth :: Yesod m => Comment -> GWidget s m ()
+showCommentAuth comment =  do
+    commentContent   <- lift . markdownToHtml $ content comment
+    commentTimestamp <- return . flip humanReadableTimeDiff (timeStamp comment) =<< liftIO getCurrentTime
+    let anchor = "#comment_" ++ show (commentId comment)
+    addHamlet [hamlet|
+        <p>
+            <a href="#{anchor}" id="#{anchor}">#{commentTimestamp}
+            , #{userName comment} wrote:
+
+        <blockquote>
+            #{commentContent}
+        |]
+
+-- <https://github.com/snoyberg/haskellers/blob/master/Haskellers.hs>
+-- <https://github.com/snoyberg/haskellers/blob/master/LICENSE>
+humanReadableTimeDiff :: UTCTime     -- ^ current time
+                      -> UTCTime     -- ^ old time
+                      -> String
+humanReadableTimeDiff curTime oldTime =
+    helper diff
+  where
+    diff    = diffUTCTime curTime oldTime
+
+    minutes :: NominalDiffTime -> Double
+    minutes n = realToFrac $ n / 60
+
+    hours :: NominalDiffTime -> Double
+    hours   n = minutes n / 60
+
+    days :: NominalDiffTime -> Double
+    days    n = hours n / 24
+
+    weeks :: NominalDiffTime -> Double
+    weeks   n = days n / 7
+
+    years :: NominalDiffTime -> Double
+    years   n = days n / 365
+
+    i2s :: RealFrac a => a -> String
+    i2s n = show m where m = truncate n :: Int
+
+    old = utcToLocalTime utc oldTime
+
+    trim = f . f where f = reverse . dropWhile isSpace
+
+    dow           = trim $! formatTime defaultTimeLocale "%l:%M %p on %A" old
+    thisYear      = trim $! formatTime defaultTimeLocale "%b %e" old
+    previousYears = trim $! formatTime defaultTimeLocale "%b %e, %Y" old
+
+    helper  d | d < 1          = "just now"
+              | d < 60         = i2s d ++ " seconds ago"
+              | minutes d < 2  = "one minute ago"
+              | minutes d < 60 =  i2s (minutes d) ++ " minutes ago"
+              | hours d < 2    = "one hour ago"
+              | hours d < 24   = "about " ++ i2s (hours d) ++ " hours ago"
+              | days d < 5     = "at " ++ dow
+              | days d < 10    = i2s (days d)  ++ " days ago"
+              | weeks d < 2    = i2s (weeks d) ++ " week ago"
+              | weeks d < 5    = i2s (weeks d)  ++ " weeks ago"
+              | years d < 1    = "on " ++ thisYear
+              | otherwise      = "on " ++ previousYears
